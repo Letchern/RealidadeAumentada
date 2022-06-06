86874e040af5b8e2f17ed7fc1513d2a676f8a811
JulianaGSantana <jugsantana@icloud.com>
2022-06-06 17:39:28 -0300

    HomeMockada

--- juju/juju/HomeViewController.swift
+++ juju/juju/HomeViewController.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    @IBOutlet weak var emotionImageView: UIImageView?
+    @IBOutlet weak var startButton: UIButton?
+    @IBOutlet weak var howToButton: UIButton?
+    @IBOutlet weak var KidImageView: UIImageView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        howToButton?.setTitle("", for: .normal)
+        
+        let buttonHow = UIImage(named: "howbutton")
+        howToButton?.setImage(buttonHow?.withRenderingMode(.alwaysOriginal), for: .normal)
+        
+        
+    }
+    
+}
